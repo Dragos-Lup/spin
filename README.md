55bdb04c5d8c0e44ccd47f5e2ab22f94b454803d
Dragos <dragosglup@gmail.com>
2025-01-03 17:31:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spin
+ A game made for the boss rush game jam 2025, with the spin theme
---
+++ scripts/main_character.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 3000.0
+const MAX_SPEED = 1000.0
+const DASH_SPEED = 2000.0
+const FRICTION = 20.0 ## calling this friction is evil, it's the value move_toward uses.
+const BOUNCY = .8
+var canDash = true
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("left", "right", "up", "down").normalized()
+	var target_vel = (direction * SPEED).clampf(-MAX_SPEED, MAX_SPEED)
+	
+	if Input.is_action_just_pressed("dash") && canDash:
+		var dash_vector = (get_global_mouse_position() - global_position).normalized()
+		velocity = dash_vector * DASH_SPEED
+		canDash = false
+		$dashTimer.start()
+	
+	velocity = velocity.move_toward(target_vel, FRICTION)
+	
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal()) * BOUNCY
+
+func _on_timer_timeout() -> void:
+	canDash = true
