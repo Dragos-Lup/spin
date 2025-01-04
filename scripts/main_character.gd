ba402faf321b8adc06a3b20ff09f5115f09996fa
Dragos <dragosglup@gmail.com>
2025-01-03 22:52:32 -0500

    erm nothing dont worry about

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -5,13 +5,12 @@ var MAX_SPEED = 1000.0
 const DASH_SPEED = 2000.0
 const FRICTION = 20.0 ## calling this friction is evil, it's the value move_toward uses.
 const BOUNCY = .8
+const DASH_SLOW = .2
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 
 @onready var state_machine = animation_tree.get("parameters/playback")
 
-var canDash = true
-var dashing = false
 var dash_start_time = 0
 
 func _physics_process(delta: float) -> void:
@@ -21,13 +20,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("dash"): 
 		dash_start_time = Time.get_ticks_msec()
 		state_machine.travel("jump")
-		MAX_SPEED *= .25
-		canDash = false
-		dashing = true
+		MAX_SPEED *= DASH_SLOW
 		
 	if Input.is_action_just_released("dash"):
-		assert(MAX_SPEED == 250)  ## This will need to be removed in final build
-		MAX_SPEED *= 4
+		assert(MAX_SPEED == 200)  ## This will need to be removed in final build
+		MAX_SPEED *= 1 / DASH_SLOW
 		var chargetime = Time.get_ticks_msec() - dash_start_time
 		if (chargetime) > 850:
 			var dash_vector = (get_global_mouse_position() - global_position).normalized()
