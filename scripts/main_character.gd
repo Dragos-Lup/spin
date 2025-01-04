1bee9d782ce362466c7c8f849deee9d5e9d49c73
Dragos <dragosglup@gmail.com>
2025-01-03 20:12:43 -0500

    animation player stuff
    I'm realizing now i've literally never used the animation tree on godot uhhhhh figured it out though yippee

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -6,19 +6,29 @@ const DASH_SPEED = 2000.0
 const FRICTION = 20.0 ## calling this friction is evil, it's the value move_toward uses.
 const BOUNCY = .8
 
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+@onready var state_machine = animation_tree.get("parameters/playback")
+
 var canDash = true
+var dash_start_time = 0
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "up", "down").normalized()
 	var target_vel = (direction * SPEED).clampf(-MAX_SPEED, MAX_SPEED) #This is how fast we want to go
 	
 	if Input.is_action_just_pressed("dash") && canDash: 
-		var dash_vector = (get_global_mouse_position() - global_position).normalized()
-		velocity = dash_vector * DASH_SPEED
+		dash_start_time = Time.get_ticks_msec()
+		print("JUMPED")
+		state_machine.travel("jump")
+		#var dash_vector = (get_global_mouse_position() - global_position).normalized()
+		#velocity = dash_vector * DASH_SPEED
 		canDash = false
-		$dashTimer.start()
+		%dashTimer.start()
 	
 	velocity = velocity.move_toward(target_vel, FRICTION)
+	
+	print($AnimationTree/AnimationPlayer.current_animation)
 	
 	var collision = move_and_collide(velocity * delta)
 	if collision:
