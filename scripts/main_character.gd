756325680f366fdb4899f1f77a9d1fa4f21c55bd
Dragos <dragosglup@gmail.com>
2025-01-06 19:10:49 -0500

    added bad particle system
    I really dont like it, but it tracks recent movement sorta?

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -7,16 +7,18 @@ const FRICTION = 20.0 ## calling this friction is evil, it's the value move_towa
 const BOUNCY = .8
 const DASH_SLOW = .2
 
+@onready var spin_bar: ProgressBar = %SpinBar
 @onready var animation_tree: AnimationTree = $AnimationTree
-
 @onready var state_machine = animation_tree.get("parameters/playback")
+
+
 
 var dash_start_time = 0
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "up", "down").normalized()
-	var target_vel = (direction * SPEED).clampf(-MAX_SPEED, MAX_SPEED) #This is how fast we want to go
-	
+	var target_vel = (direction * SPEED).limit_length(MAX_SPEED) #This is how fast we want to go
+
 	if Input.is_action_just_pressed("dash"): 
 		dash_start_time = Time.get_ticks_msec()
 		state_machine.travel("jump")
@@ -29,9 +31,9 @@ func _physics_process(delta: float) -> void:
 		if (chargetime) > 850:
 			var dash_vector = (get_global_mouse_position() - global_position).normalized()
 			velocity = dash_vector * (DASH_SPEED * chargetime/1000)
-		
 	velocity = velocity.move_toward(target_vel, FRICTION)
-	
+	print(velocity.length())
+	spin_bar.value = velocity.length() / 10
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.bounce(collision.get_normal()) * BOUNCY
