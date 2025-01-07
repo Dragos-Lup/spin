981505c29110387d437be280480fc5b6207d06bf
Dragos <dragosglup@gmail.com>
2025-01-07 00:09:21 -0500

    holy shit im the goat
    implemented encircle polygon and circle making

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -10,10 +10,16 @@ const DASH_SLOW = .2
 @onready var spin_bar: ProgressBar = %SpinBar
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
-
-
+const Encircle = preload("res://scenes/encirclePolygon.tscn")
+var move_array: PackedVector2Array = PackedVector2Array()
 
 var dash_start_time = 0
+
+func _init() -> void:
+	#move_array.append(self.position)
+	#move_array.append(self.position)
+	if (intersect(Vector2(0,5), Vector2(0,-5), Vector2(-5,0), Vector2(5,0))):
+		print("well these two definitely do...")
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "up", "down").normalized()
@@ -32,8 +38,41 @@ func _physics_process(delta: float) -> void:
 			var dash_vector = (get_global_mouse_position() - global_position).normalized()
 			velocity = dash_vector * (DASH_SPEED * chargetime/1000)
 	velocity = velocity.move_toward(target_vel, FRICTION)
-	print(velocity.length())
+	
 	spin_bar.value = velocity.length() / 10
+	
+	
+	
 	var collision = move_and_collide(velocity * delta)
+	
 	if collision:
 		velocity = velocity.bounce(collision.get_normal()) * BOUNCY
+		
+
+func _on_location_timer_timeout() -> void:
+	var p : Vector2 = self.position
+	move_array.append(p)
+	if (move_array.size() > 30):
+		move_array.remove_at(0)
+	if (move_array.size() > 3):
+		var A = move_array[move_array.size() - 1]
+		var B = move_array[move_array.size() - 2]
+		for i in range(0, move_array.size() - 1):
+			if (intersect(A, B, move_array[i], move_array[i+1])):
+				print(A,", ",B,", ",move_array[i], ", " , move_array[i+1])
+				print("INTERSECTED")
+				var new_polygon = Encircle.instantiate()
+				
+				new_polygon.set_polygon(move_array.slice(i+1,-1))
+				new_polygon.set_color(Color(1,0,0,.5))
+				get_tree().root.add_child(new_polygon)
+				break
+			#else:
+				#print("NOPE")
+	
+
+func ccw(A : Vector2,B : Vector2,C : Vector2):
+	return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x)
+
+func intersect(A : Vector2,B : Vector2,C : Vector2,D : Vector2):
+	return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
