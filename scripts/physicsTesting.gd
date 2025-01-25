578dad401f3b1d099764f576d315b3d3dd83230e
Dragos <dragosglup@gmail.com>
2025-01-25 02:36:41 -0500

    dont like integrate forces

--- scripts/physicsTesting.gd
+++ scripts/physicsTesting.gd
@@ -4,20 +4,24 @@ var target: Vector2 = Vector2(500,500)
 var t_check: bool = false
 var start: Vector2
 
-var MAX_SPEED: float = 500
+var MAX_SPEED: float = 800
 var MAX_FORCE: float = 400000
 var SLOW_RADIUS: float = 100
 
 var proportionalGain: float = 1.0
 var integralGain: float = 0.0
-var derivativeGain: float = 1.0
+var derivativeGain: float = 50
 var errorLast: Vector2 = Vector2.ZERO
+var valueLast: Vector2 = Vector2.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+
+func _integrate_forces(_state):
+	linear_velocity = linear_velocity.limit_length(MAX_SPEED)
 
 func _physics_process(_delta: float) -> void:
 	var force = Vector2.ZERO
@@ -30,14 +34,17 @@ func _physics_process(_delta: float) -> void:
 
 	var error = target - self.transform.get_origin()
 	var P = proportionalGain * error
-	var try = error.normalized() * MAX_SPEED
-	if error.length() < SLOW_RADIUS:
-		force = P
-	else:
-		force = try
+	var errorRoC = (error - errorLast)# / delta
+	errorLast = error
+	var valueRoC = self.transform.get_origin() - valueLast #/ delta
+	valueLast = self.transform.get_origin()
+	var D = derivativeGain * errorRoC
+	print(D)	
+	force = P + D
 	#if target:
 	#	force = seek(target)
 	apply_central_force(force)
+	# print(linear_velocity)
 
 func seek(_seek_target: Vector2) -> Vector2:
 	var force = 2
